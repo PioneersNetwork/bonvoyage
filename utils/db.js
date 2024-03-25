39ce869f6befdc1834f72b36209c15b9fd92501f
PioneersNetwork <33698879+PioneersNetwork@users.noreply.github.com>
2024-03-25 05:09:31 +0300

    0.0.14

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2";
 const connection = mysql
   .createPool({
-    host: "bon-voyage.services",
+    host: "dashboard.bon-voyage.services",
     user: "bonvoyage",
     database: "bonvoyag_bonvoyag",
     password:"&&cHF2_aB]=4"
